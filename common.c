b85dceff4760833c7f7435a958401a4b7beb3696
lcc <faty2009@foxmail.com>
2019-12-04 17:14:04 +0800

    上次提交忘了增加关键文件了。

---
+++ common.c
@@ -0,0 +1,200 @@
+#include "common.h"
+
+#define random(x) (rand()%x)
+
+#define PWD_DIC_SEED	7758
+#define MAX_DIC_CNT		255
+
+
+static int s_pwd_dic[MAX_DIC_CNT] = {0};
+static int s_dic_pwd[MAX_DIC_CNT] = {0};
+
+static void generate_pwd_dic()
+{
+	int cnt = 0;
+	unsigned char rad;
+
+	for (cnt=0; cnt<MAX_DIC_CNT; cnt ++) {
+		s_pwd_dic[cnt] = -1;
+		s_dic_pwd[cnt] = -1;
+	}
+	cnt = 0;
+
+	srand(PWD_DIC_SEED);
+	while (cnt<MAX_DIC_CNT) {
+		rad = random(MAX_DIC_CNT);
+
+		/** we already have such a num */
+		if (-1!=s_dic_pwd[rad]) 
+			continue;
+
+		s_pwd_dic[cnt] = rad;
+		s_dic_pwd[rad] = cnt;
+
+		cnt ++;
+	}
+}
+
+static void pwd_dic_data(const char *data, int len, char *o_result)
+{
+	int i = 0;
+	for (i=0; i<len; i++) {
+		o_result[i] = s_pwd_dic[data[i] ];
+	}
+}
+
+static void dic_pwd_data(const char *data, int len, char *o_result)
+{
+	int i=0;
+	for (i=0; i<len; i++) {
+		o_result[i] = s_dic_pwd[s_pwd_dic[data[i] ] ];
+	}
+}
+
+long x_send(int sock, const void *buf, size_t n, int flag)
+{
+    char *res = malloc(n);
+    assert(res);
+    memset(res, 0, n);
+
+    pwd_dic_data((const char *)buf, n, res);
+    long ret = send(sock, res, n, flag);
+    free(res);
+
+    return ret;
+}
+long x_recv(int sock, void *buf, size_t n, int flag)
+{
+    char *raw = malloc(n);
+    assert(raw);
+    memset(raw, 0, n);
+
+    long ret = recv(sock, raw, n, flag);
+    if (ret>0){
+        memset(buf, 0, n);
+        dic_pwd_data(raw, ret, buf);
+    }
+    free(raw);
+    return ret;
+}
+
+void x_send_recv_init()
+{
+    generate_pwd_dic();
+}
+
+
+
+
+
+
+
+
+void add_ip(struct ip_root_s *root, const char *ip)
+{
+	struct ip_list_s *node = NULL;
+
+	if (NULL==root->head) {
+		root->head = malloc(sizeof(struct ip_list_s));
+		assert(root->head);
+		root->head->next = NULL;
+		/** now head is also tail */
+		root->tail = root->head;
+	} else {
+		node = malloc(sizeof(struct ip_list_s));
+		assert(node);
+		node->next = NULL;
+		/** connect node to list tail */
+		root->tail->next = node;
+		/** now node is tail */
+		root->tail = node;		
+	}
+    debug(" -- add ip: \"%s\"\n", ip);
+	strncpy(root->tail->ip, ip, MAX_IP_LEN);
+}
+
+void free_ip_list(struct ip_root_s *root)
+{
+	struct ip_list_s *node = root->head;
+	while (node) {
+		free (node);
+		node = node->next;
+	}
+	root->head = NULL;
+	root->tail = NULL;
+}
+
+
+
+#include <sys/time.h>
+long get_cur_ms()
+{
+    struct timeval tv;
+    struct timezone tz;
+
+    gettimeofday(&tv, &tz);
+
+    return (long)(tv.tv_sec*1000 + tv.tv_usec/1000);
+}
+
+
+
+int forward_data(int sock, int real_server_sock)
+{
+    char recv_buffer[BUFF_SIZE] = {0};
+
+    fd_set fd_read;
+    struct timeval time_out;
+
+    time_out.tv_sec = TIME_OUT;
+    time_out.tv_usec = 0;
+
+    int ret = 0;
+    long cur = get_cur_ms();
+    while(1) {
+        FD_ZERO(&fd_read);
+        FD_SET(sock, &fd_read);
+        FD_SET(real_server_sock, &fd_read);
+
+        if (get_cur_ms()-cur >TIME_OUT_MS) {
+            //debug("sock %d timeout\n", sock);
+            break;
+        } 
+        ret = select((sock > real_server_sock ? sock : real_server_sock) + 1, &fd_read, NULL, NULL, &time_out);
+        if (-1 == ret) {
+            debug("%d select socket[%d, %d] error, %m\n", (unsigned char)pthread_self(), sock, real_server_sock);
+            break;
+        } else if (0 == ret) {
+            continue;
+        }
+        if (FD_ISSET(sock, &fd_read)) {
+            //printf("client can read!\n");
+            memset(recv_buffer, 0, BUFF_SIZE);
+            ret = x_recv(sock, recv_buffer, BUFF_SIZE, 0);
+            if (ret <= 0) {
+                debug("sock %d x_recv from client error, %m\n", sock);
+                break;
+            }
+            ret = x_send(real_server_sock, recv_buffer, ret, 0);
+            if (ret == -1) {
+                debug("sock %d x_send data to real server failed, %m\n", sock);
+                break;
+            }
+        } else if (FD_ISSET(real_server_sock, &fd_read)) {
+            //printf("real server can read!\n");
+            memset(recv_buffer, 0, BUFF_SIZE);
+            ret = x_recv(real_server_sock, recv_buffer, BUFF_SIZE, 0);
+            if (ret <= 0) {
+                debug("real server sock x_recv data failed, %m\n");
+                break;
+            }
+            ret = x_send(sock, recv_buffer, ret, 0);
+            if (ret == -1) {
+                perror("x_send data to client error");
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ common.h
@@ -0,0 +1,100 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <stdio.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <pthread.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <assert.h>
+
+#define BUFF_SIZE 10240
+
+#define TIME_OUT 5   // seconds
+#define TIME_OUT_MS 10000
+
+#define WHITE_IP_LIST       "white_ip_list.txt"
+
+#define		debug(fmt, ...)			printf((fmt), ##__VA_ARGS__)
+
+static pthread_mutex_t s_mutex_lock = PTHREAD_MUTEX_INITIALIZER;
+#define LOCK()      pthread_mutex_lock(&s_mutex_lock)
+#define UNLOCK()    pthread_mutex_unlock(&s_mutex_lock)
+
+
+#define MAX_IP_LEN 18
+struct ip_list_s {
+	char		ip[MAX_IP_LEN];
+	struct ip_list_s *next;
+};
+struct ip_root_s {
+	struct ip_list_s *head;
+	struct ip_list_s *tail;
+};
+
+
+
+
+/** 统计当前存活线程的数目 */
+static int s_thread_cnt = 0;
+#define     threads_cnt_inc()   do{LOCK();s_thread_cnt++;UNLOCK();}while(0)
+#define     threads_cnt_dec()   do{LOCK();s_thread_cnt--;UNLOCK();}while(0)
+static int threads_cnt()   
+{
+    LOCK();
+    int ret = s_thread_cnt;
+    UNLOCK();
+    return ret;
+}
+
+/** 用于判断线程是否已经创建完毕，防止地址传的参数被修改 */
+static int s_create_thread_flag = 0;
+static void create_thread_start(int sock)
+{
+    struct timeval timeout={3,0};//3s
+    if (0!=setsockopt(sock,SOL_SOCKET,SO_SNDTIMEO,&timeout,sizeof(timeout))) {
+        debug("set sock %d opt failed, %m\n", sock);
+    }
+    if (0!=setsockopt(sock,SOL_SOCKET,SO_RCVTIMEO,&timeout,sizeof(timeout))) {
+        debug("set sock %d opt failed, %m\n", sock);
+    }
+    //debug("create thread for sock %d\n", sock);
+    LOCK();
+    s_create_thread_flag = 0;
+    UNLOCK();
+}
+static void thread_created(int sock)
+{
+    LOCK();
+    s_create_thread_flag = 1;
+    UNLOCK();
+    //debug("thread for sock %d already created\n", sock);
+}
+static int is_thread_created()
+{
+    LOCK();
+    int ret = s_create_thread_flag;
+    UNLOCK();
+    return ret;
+}
+
+long get_cur_ms();
+
+void x_send_recv_init();
+long x_recv(int sock, void *buf, size_t n, int flag);
+long x_send(int sock, const void *buf, size_t n, int flag);
+
+void add_ip(struct ip_root_s *root, const char *ip);
+void free_ip_list(struct ip_root_s *root);
+
+int forward_data(int sock, int real_server_sock);
+
+#endif
